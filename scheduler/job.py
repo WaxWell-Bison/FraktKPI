22214fbd7beee6b3c10fe3814adc1226a6e4afe1
Olivier Buez <olivierbuez@gmail.com>
2021-12-26 16:05:56 +0100

    refactor api calls + fix circulating supply

--- scheduler/job.py
+++ scheduler/job.py
@@ -3,28 +3,28 @@ from datetime import date, datetime
 
 from pymongo import MongoClient
 
-from ftx import Client
-
 import json
 import requests
 import math
 import os
 import signal
 
-team_wallet = [
-    "DRHLza6iRbcFqujosgwnHiVxr2AMYdSaRHyFV4XVfG4c",
-    "Ec93KxGJjwW6PBhiZQbVJrFst8mYbzBZ6embKf4Pxqus",
-    "4BNZzYqB4M2oHTvWnn8ww8u5aosgjRTMQnv5WNikXLAf",
-    "GwXKptwrXvFQ2VjcjXc9JwVzmAeGvPH9ogtq54AaDZcm",
-    "BtgqY3wS4QiFVETAN3vQidPawaYweyGcrGmA9CcYDr4r",
-    "4hMdAptCtcB3unnX1ucL6epVnpsCayGArg2iKPqCgohf",
+team_address = [
+    "JGPfKwykSgNJoPGSXD39ZxQtptGmHSvAiem7iRjK7sn", #Team locked
+    "FGXMsYgfvk8pH6UpG4rNGfQznxYnfA2AjihCc9j1b54p", #Team locked
+    "BawTL3vwEc3Aoi5mugwP6b7ZQ5dH5b7hfRKWDG7kZMc5", #Team locked
+    "CaGvtrQj71GkY9RXHzDerhp7iKdBD8iVr6uWEhVuMcm", #FRKT-SOL Raydium farm
+    "2fWTvWhYDQ7nEhweStAThXzBipDw9RuDw86Wb2R4Mu2c", #NFTs Staking reward
+    "8JLnRhwckPfH9kG7LhKu4t3pPg1Y6rZJvuaNyAgC73b2", #Advisors allocation
+    "5TPJHNJ4ZeJ6BSbZ83R4ZdA7R3Tsf3icgxQVaWjEExn7",  #FRKT-USDC Raydium farm
+    "3NLY7Ww3HWCSogwL4zZmapSJQGrwMWinuMtyFM5vzCDg", #FRKT-USDC Aldrin farm
+    "C2XYEYJKaqzqe1acqRmYiwhV3iviDetwUxJ4T3i2PhJM" #FRKT stacking
     ]
 
-burn_wallet = "1nc1nerator11111111111111111111111111111111"
-fusion_pool = "FE5nRChviHFXnUDPRpPwHcPoQSxXwjAB5gdPFJLweEYK"
+burn_address = "2F1r1go71SUK4N2rn4MYQ4N2ifmux8QUAuooHguTfK8S"
+fusion_pool = "8inqBe7D12XJ6tMAzpLCGYpjazWFXG1Ue5q3UZ6X1FM3"
 token_address = 'ErGB9xa24Szxbk1M28u2Tx8rKPqzL6BroNkkzk5rG4zj'
 
-f_client = Client(os.environ['FTX_KEY'], os.environ['FTX_SECRET'])
 db = MongoClient('mongo', 27017, username=os.environ['MONGO_USER'], password=os.environ['MONGO_PASSWORD']).db
 
 head = {
@@ -32,29 +32,30 @@ head = {
 }
 
 def get_pool_data():
+    to_return = {'h24_volume': 0, 'price': 0}
+    markets = ["FRKT-USDC", "FRKT-SOL"]
     for pair in requests.get('https://api.raydium.io/pairs').json():
-        if pair.get("name","") == "FRKT-SOL" and pair.get("market","") == fusion_pool:
-            return {
-                'liquidity': pair['liquidity'],
-                'frkt': pair['token_amount_coin'],
-                'sol': pair['token_amount_pc'],
-                'lp': pair['token_amount_lp'],
-                'h24_volume': pair['volume_24h'],
-                'price': pair['price'] * f_client.get_price_pair('SOL/USDT'),
-                'apy': pair['apy']
-            }
+        if pair.get("market", "") == fusion_pool:
+            to_return['price'] = pair['price']
+        if pair.get("name","") in markets:
+            to_return['h24_volume'] += pair['volume_24h']
+    return to_return
 
 def get_supply_data():
-    supply = requests.get(f"https://api.solscan.io/account?address={token_address}", headers=head).json()['data']['tokenInfo']
-    tech_supply = float(supply['supply']) / math.pow(10, int(supply['decimals']))
-    burned_supply = 0
-    team_supply = 0
+    def to_decimal(base, exp):
+        return float(base) / math.pow(10, int(exp))
 
-    for holder in requests.get(f"https://api.solscan.io/token/holders?token={token_address}&offset=0&size=20", headers=head).json()['data']['result']:
-        if holder['owner'] in team_wallet:
-            team_supply += float(holder['amount']) / math.pow(10,int(holder['decimals']))
-        if holder['owner'] == burn_wallet:
-            burned_supply = float(holder['amount']) / math.pow(10,int(holder['decimals']))
+    def get_account_amount(account):
+        amount = requests.get(f"https://api.solscan.io/account?address={account}", headers=head).json()['data']['tokenInfo']['tokenAmount']
+        return to_decimal(amount['amount'], amount['decimals'])
+    
+    def get_total_supply(token_address):
+        supply = requests.get(f"https://api.solscan.io/account?address={token_address}", headers=head).json()['data']['tokenInfo']
+        return to_decimal(supply['supply'], supply['decimals'])
+
+    tech_supply = get_total_supply(token_address)
+    burned_supply = get_account_amount(burn_address)
+    team_supply = sum([get_account_amount(wallet) for wallet in team_address])
 
     return {
         'burned': burned_supply,
@@ -86,15 +87,16 @@ def populate(sig=None,stack=None):
     data = {'pool':{}, 'supply':{}, 'market': {}, 'nft': {}, 'fraktion':{}, 'frkt': {}, 'timestamp': datetime.now()}
 
     data['pool'] = get_pool_data()
-    data['supply'] = get_supply_data()
 
     data['holders'] = get_holders_data()
-    data['market']['cap'] = data['supply']['circulating']  * data['pool']['price']
-    data['market']['diluated_cap'] = data['supply']['total'] * data['pool']['price']
 
     data['nft'] = get_nft_data()
     data['fraktion'] = get_fraktion_data()
     data['frkt'] = get_stakers_data()
+    data['supply'] = get_supply_data()
+    data['supply']['circulating'] += data['frkt']['staked']
+    data['market']['cap'] = data['supply']['circulating']  * data['pool']['price']
+    data['market']['diluated_cap'] = data['supply']['total'] * data['pool']['price']
 
     db.stats.insert_one(data)
 
